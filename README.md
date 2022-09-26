8f1392dd26f8b871b5c7e58a5ec8bfb33eea245a
baba-s <shotababa1206@gmail.com>
2022-09-26 21:22:02 +0900

    add

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# Kogane.ObjectInstantiateExtensionMethods
+# Kogane Object Instantiate Extension Methods
+
+Object.Instantiate の拡張メソッド
+
+## 使用例
+
+```csharp
+using Kogane;
+using UnityEngine;
+
+public sealed class Example : MonoBehaviour
+{
+    public GameObject m_prefab;
+
+    private void Awake()
+    {
+        var r1 = m_prefab.Instantiate();
+        var r2 = m_prefab.Instantiate( Vector3.zero );
+        var r3 = m_prefab.Instantiate( Vector3.zero, Quaternion.identity );
+        var r4 = m_prefab.Instantiate( Vector3.zero, Quaternion.identity, transform );
+        var r5 = m_prefab.Instantiate( transform );
+        var r6 = m_prefab.Instantiate( transform, true );
+    }
+}
+```
---
+++ Runtime/ObjectInstantiateExtensionMethods.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+
+namespace Kogane
+{
+    public static class ObjectInstantiateExtensionMethods
+    {
+        public static T Instantiate<T>( this T self ) where T : Object
+        {
+            return Object.Instantiate( self );
+        }
+
+        public static T Instantiate<T>
+        (
+            this T  self,
+            Vector3 position
+        ) where T : Object
+        {
+            return Object.Instantiate
+            (
+                original: self,
+                position: position,
+                rotation: Quaternion.identity
+            );
+        }
+
+        public static T Instantiate<T>
+        (
+            this T     self,
+            Vector3    position,
+            Quaternion rotation
+        ) where T : Object
+        {
+            return Object.Instantiate
+            (
+                original: self,
+                position: position,
+                rotation: rotation
+            );
+        }
+
+        public static T Instantiate<T>
+        (
+            this T     self,
+            Vector3    position,
+            Quaternion rotation,
+            Transform  parent
+        ) where T : Object
+        {
+            return Object.Instantiate
+            (
+                original: self,
+                position: position,
+                rotation: rotation,
+                parent: parent
+            );
+        }
+
+        public static T Instantiate<T>
+        (
+            this T    self,
+            Transform parent
+        ) where T : Object
+        {
+            return Object.Instantiate
+            (
+                original: self,
+                parent: parent
+            );
+        }
+
+        public static T Instantiate<T>
+        (
+            this T    self,
+            Transform parent,
+            bool      worldPositionStays
+        ) where T : Object
+        {
+            return Object.Instantiate
+            (
+                original: self,
+                parent: parent,
+                worldPositionStays: worldPositionStays
+            );
+        }
+    }
+}
